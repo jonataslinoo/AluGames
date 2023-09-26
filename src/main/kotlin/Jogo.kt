8289a191e284449c7fe556194cad713d6a629c00
jonataslinoo <jonatas.linoo@gmail.com>
2023-09-25 22:40:39 -0300

    Adicionado requisição Http para comunicar com a API SHARK e adicionado resposta Http onde receberemos as informações dos jogos obtidas da requisição. Adicionado data classe Jogo para receber as informações dos jogos que queremos apresentar na tela. Adicionado data classe InfoJogo para recuperar os dados da API SHARK e fazer a conversão do json recebido para objeto Jogo. Adicionado data classe InfoApiShark para fazer a serialização dos dados recebidos no json e não ter que ficar serializando os nomes dentro do construtor da classe de Jogo.

---
+++ src/main/kotlin/Jogo.kt
@@ -0,0 +1,14 @@
+data class Jogo(
+    val titulo: String,
+    val capa: String
+) {
+    val descricao = ""
+
+    override fun toString(): String {
+        return "Meu Jogo: \n" +
+                "Título: $titulo, \n" +
+                "Capa $capa ,\n" +
+                "Descrição: $descricao \n"
+    }
+}
+
